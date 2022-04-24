11a99a10318bb51cdb7d527ed33e36bc1ad79952
Rizbir Khan <fahmid_98@outlook.com>
2022-04-24 19:52:36 +0100

    Adding option to read json data, creating requirements.txt file

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("FHIR conversoin").getOrCreate()
 
-df = spark.read.json("./data")
+df = spark.read.option("multiline", "true").json("./data")
 
 df.show(10)
