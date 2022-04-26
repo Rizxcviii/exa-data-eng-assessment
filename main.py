97dcfdacf2863d71f87a8277d40a2cbf3d27158b
Rizbir Khan <fahmid_98@outlook.com>
2022-04-26 17:14:08 +0100

    using rdds to achieve computation

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, explode, from_json
+from pyspark.sql.types import ArrayType, StringType, StructField, StructType
 
 spark = SparkSession.builder.appName("FHIR conversoin").getOrCreate()
 
-df = spark.read.option("multiline", "true").json("./data")
+data = spark.read.option("multiline", "true").json("./data").rdd
 
-df.show(10)
+mappedLines = data.map(lambda x: x.asDict())
+
+entryLines = mappedLines.map(lambda x: x["entry"])
+
+flattendEntries = entryLines.flatMap(lambda x: x)
+
+requiredData = flattendEntries.map(lambda x: (x["fullUrl"], x["resource"]["id"]))
+
+requiredData.saveAsTextFile("./output")
